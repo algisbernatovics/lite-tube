92adfcbd0e659cbb6fea2a092ed7d939f5688da2
Algis @ home <algis.bernatovics@gmail.com>
2024-03-30 03:46:36 +0200

    add most accessable player

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -28,18 +28,55 @@
     @if(isset($videos) && count($videos) > 0)
         <div class="grid gap-4 md:gap-8 grid-cols-1 md:grid-cols-3">
             @foreach($videos as $video)
-                <a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 p-4">
+                <a href="#" onclick="openVideoModal('{{ $video->id->videoId }}', '{{ $video->snippet->title }}')" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 p-4">
                     <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="{{ $video->snippet->thumbnails->medium->url }}" alt="{{$video->snippet->title}}">
                     <div class="flex flex-col justify-between mt-4 leading-normal">
                         <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{$video->snippet->title}}</p>
                     </div>
                 </a>
             @endforeach
+
         </div>
     @else
         <p class="text-gray-600">No trending videos available.</p>
     @endif
+
+    <!-- Modal -->
+    <div id="videoModal" class="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 hidden">
+        <div class="bg-black rounded-lg shadow-lg p-8">
+            <div id="videoContainer"></div>
+            <div class="flex justify-end mt-4">
+                <button onclick="closeVideoModal()" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mr-4">Close</button>
+            </div>
+        </div>
+    </div>
+
 </div>
+
+<!-- JavaScript -->
+<script>
+    function openVideoModal(videoId, title) {
+        var iframe = document.createElement('iframe');
+        iframe.setAttribute('width', '560');
+        iframe.setAttribute('height', '315');
+        iframe.setAttribute('src', 'https://www.youtube.com/embed/' + videoId + '?modestbranding=1');
+        iframe.setAttribute('frameborder', '0');
+        iframe.setAttribute('allow', 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture');
+        iframe.setAttribute('allowfullscreen', 'true');
+
+        var container = document.getElementById('videoContainer');
+        container.innerHTML = '';
+        container.appendChild(iframe);
+
+        var modal = document.getElementById('videoModal');
+        modal.classList.remove('hidden');
+    }
+    function closeVideoModal() {
+        var modal = document.getElementById('videoModal');
+        modal.classList.add('hidden');
+    }
+
+</script>
 
 </body>
 </html>
